ae7888cf6bd83809d84909b338be53df990a434e
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-13 19:04:55 +0600

    ssrgg

---
+++ pages/disha.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Home.module.css";
+const Disha = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div>
+          <h2>Once a famous Manus said: i kittam...</h2>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Disha;
