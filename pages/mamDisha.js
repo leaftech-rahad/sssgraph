a26ea2fee694ba9671aebd63f58dc324184f3404
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-13 19:22:57 +0600

    ssrgg

--- pages/mamDisha.js
+++ pages/mamDisha.js
@@ -1,14 +1,14 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-const Disha = () => {
+const ManDisha = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div>
-          <h2>Once a famous Manus said: i kittam...</h2>
-          <Link href="/mamDisha">
+          <h2>Yes we miss you too...bandhobi</h2>
+          <Link href="/disha">
             <button>
-              <a>click me</a>
+              <a>Go back</a>
             </button>
           </Link>
         </div>
@@ -17,4 +17,4 @@ const Disha = () => {
   );
 };
 
-export default Disha;
+export default ManDisha;
