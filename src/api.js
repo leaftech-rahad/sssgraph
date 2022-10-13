fe3fc2d9a8ff095dbd9f5b8989ea2fc61be973bd
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-13 14:15:59 +0600

    ssr2

--- src/api.js
+++ src/api.js
@@ -4,7 +4,9 @@ import { QueryClient } from "react-query";
 import { getSdk } from "./generated/graphql";
 
 // const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
-const gqlClient = new GraphQLClient(`${process.env.VERCEL_URL}/api/graphql`);
+const gqlClient = new GraphQLClient(
+  `https://${process.env.VERCEL_URL}/api/graphql`
+);
 export const { allusers } = getSdk(gqlClient);
 
 export const queryClient = new QueryClient({
