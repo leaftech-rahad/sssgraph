a4b3d35c09e5e220356065294a30180f3fd5f78d
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-25 21:23:37 +0600

    mutation

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,23 +2,36 @@
 import { ApolloServer } from "apollo-server-micro";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import Cors from "micro-cors";
 import { Query } from "../../src/graphql/resolvers/Query.js";
 import { Mutation } from "../../src/graphql/resolvers/Mutation";
 import { typeDefs } from "../../src/graphql/typeDefs";
 
+const cors = Cors();
 const schema = makeExecutableSchema({
   resolvers: { Query, Mutation },
   typeDefs,
 });
 
-const server = new ApolloServer({
+const apolloServer = new ApolloServer({
   schema,
-
   csrfPrevention: true,
-
   cache: "bounded",
-
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+const startServer = apolloServer.start();
+
+export default cors(async function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+  await startServer;
+
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
 });
 
 export const config = {
@@ -26,10 +39,3 @@ export const config = {
     bodyParser: false,
   },
 };
-
-const startServer = server.start();
-
-export default async function handler(req, res) {
-  await startServer;
-  await server.createHandler({ path: "/api/graphql" })(req, res);
-}
