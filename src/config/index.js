8538d8977dc3013f75c080e6edc7264d7278ff6f
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-25 21:06:49 +0600

    mutation

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const server = process.env.HOST_URL;
 export const SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${server}/api/graphql`
+    ? `https://sssgraph.vercel.app/api/graphql`
     : `http://localhost:3000/api/graphql`;
