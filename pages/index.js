02fe1ca4bd1c1a70d817e126fce098f64d0a5eb6
leaftech-rahad <leaftech.rahad@gmail.com>
2022-10-25 21:47:19 +0600

    mutation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import * as React from "react";
-// import Typography from "@mui/material/Typography";
-// import { dehydrate, useQuery } from "react-query";
-// import { queryClient, getUsers } from "../src/api";
+import Typography from "@mui/material/Typography";
+import { dehydrate, useQuery } from "react-query";
+import { queryClient, getUsers } from "../src/api";
 import Navbar from "../components/Navbar";
 
 export async function getStaticProps() {
-  // await queryClient.prefetchQuery(["getUsers"], () => getUsers());
+  await queryClient.prefetchQuery(["getUsers"], () => getUsers());
   const pages = ["Projects", "About Me", "Contact Me"];
   const settings = ["Profile", "Dashboard", "SignUp", "Login", "Logout"];
   return {
@@ -18,18 +18,18 @@ export async function getStaticProps() {
 }
 
 function Appbar(props) {
-  // const { data } = useQuery(["getUsers"], () => getUsers());
+  const { data } = useQuery(["getUsers"], () => getUsers());
 
   return (
     <>
       <Navbar {...props} />
-      {/* <div>
+      <div>
         {data?.allUsers.map((user, i) => (
           <Typography align="center" key={user.id}>
             {user.name}
           </Typography>
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
